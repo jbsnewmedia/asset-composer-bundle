475f61991d3c7911717b05c03452b74365d3d9c1
jschwind <info@juergen-schwind.de>
2025-03-07 17:24:48 +0100

    Update Rector config to target Symfony 6.0 and PHP 8.0
    This change adjusts the Rector configuration to align with Symfony 6.0 and PHP 8.0 compatibility. It ensures the tool enforces code rules suitable for these versions, supporting a consistent and updated codebase.

--- rector.php
+++ rector.php
@@ -20,9 +20,9 @@ return static function (RectorConfig $rectorConfig): void {
 
     // define sets of rules
     $rectorConfig->sets([
-        SymfonySetList::SYMFONY_71,
+        SymfonySetList::SYMFONY_60,
         SymfonySetList::SYMFONY_CODE_QUALITY,
         SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-        LevelSetList::UP_TO_PHP_82
+        LevelSetList::UP_TO_PHP_80
     ]);
 };
